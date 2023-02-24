94b4eaf74c53167f7bc905740a258f251e7d3eb9
pjluispb <124328519+pjluispb@users.noreply.github.com>
2023-02-24 16:15:34 -0400

    Update reiniciar.py

--- pages/reiniciar.py
+++ pages/reiniciar.py
@@ -2,6 +2,9 @@ import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 
 #st.sidebar_state['collapsed']
+
+clave = st.session_state['clave']
+st.write(clave)
 volver = st.button('Volver')
 if volver:
   switch_page('encuprof03')
