4750b35c021ca77f65d3a6edba479190a20aa020
pjluispb <124328519+pjluispb@users.noreply.github.com>
2023-02-24 17:36:32 -0400

    Update reiniciar.py

--- pages/reiniciar.py
+++ pages/reiniciar.py
@@ -6,12 +6,13 @@ import pandas as pd
 deta = Deta(st.secrets["deta_key"])
 
 clave = st.session_state['clave']
-st.write(clave)
+#st.write(clave)
 
 usuarios = deta.Base('usuarios')
 rec1=usuarios.get(clave)
-
-st.write(rec1)
+st.info('''Felicitaciones, los datos han sido grabados en la base de datos. 
+Aqui los puedes ver :thumb up: ''')
+#st.write(rec1)
 sdf = pd.Series(rec1)
 sdf
 
