341a4e5bab98ef7e535abb5a61d4b5f5903127b9
pjluispb <124328519+pjluispb@users.noreply.github.com>
2023-02-24 17:25:21 -0400

    Update reiniciar.py

--- pages/reiniciar.py
+++ pages/reiniciar.py
@@ -1,10 +1,18 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
+from deta import Deta
+import pandas as pd
 
-#st.sidebar_state['collapsed']
+deta = Deta(st.secrets["deta_key"])
 
 clave = st.session_state['clave']
 st.write(clave)
+
+usuarios = deta.Base('usuarios')
+rec1=usuarios.get(clave)
+
+st.write(recl)
+
 volver = st.button('Volver')
 if volver:
   switch_page('encuprof03')
