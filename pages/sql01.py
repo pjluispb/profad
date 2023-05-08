b66374b9b601ab86cb302dc8124e065cdf7c3cd4
pjluispb <124328519+pjluispb@users.noreply.github.com>
2023-05-07 22:02:26 -0400

    Update sql01.py

--- pages/sql01.py
+++ pages/sql01.py
@@ -6,7 +6,7 @@ import streamlit as st
 conn = sqlite3.connect('example.db')
 
 # Create table
-conn.execute('CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)')
+conn.execute('''CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)''')
 
 # Insert a row of data
 conn.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
