eab6144820cbaa932afcafbd8bfef337c804b44a
pjluispb <124328519+pjluispb@users.noreply.github.com>
2023-05-18 09:08:15 -0400

    Add files via upload

---
+++ pages/inicia02.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+from deta import Deta
+from PIL import Image
+import random
+from google.oauth2 import service_account
+import pygsheets
+
+deta = Deta(st.secrets["deta_key"])
+SCOPES = ('https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive')
+service_account_info = st.secrets.gcp_service_account
+my_credentials = service_account.Credentials.from_service_account_info(service_account_info, scopes = SCOPES)
+gc =pygsheets.authorize(custom_credentials=my_credentials)
+
+sh = gc.open('DonesTest')
+wks = sh.worksheet_by_title('preguntas')
+df = pd.DataFrame(wks)
+df = df.filter(items=[0,1])
+#st.write(df)
+dft = df.T
+regdft = dft.to_dict('list')
+
+with st.form('Datos Personales'):
+    evaluador = st.text_input('Introduce tu nombre')
+    cedula = st.text_input('Introduce tu # cédula')
+    aquien = evaluador
+    relacion = 'Auto' 
+    parentesco = 'A si mismo'
+    lregdft = list(regdft.values())                                   # convierto el dict de preguntas en una lista
+    random_lip = random.sample(lregdft,k=len(lregdft))                # lista random de preguntas
+
+    st.session_state['datper'] = [evaluador,aquien,relacion,parentesco]
+    st.session_state['datrpre'] = random_lip
+    st.session_state['precon'] = [0]
+    st.session_state['datperXdon'] = [[0,0]]
+    st.session_state['dones'] = [['Administración',0], ['Milagros',0], ['Hospitalidad',0], ['Aconsejar',0], ['Conocimiento',0], ['Interpretación de lenguas',0], ['Comunicación Creativa',0], ['Profecía',0], ['Sabiduría',0], ['Apostolado',0], ['Habilidad en un oficio o arte',0], ['Lenguas',0], ['Exhortación',0], ['Ayudas',0], ['Misericordia',0], ['Dar',0], ['Fe',0], ['Sanidades',0], ['Evangelismo',0], ['Discernimiento',0], ['Liderazgo',0], ['Enseñanza',0], ['Pastor-maestro',0]]
+
+    btn = st.form_submit_button('iniciar test')
+    if btn:
+        switch_page('preguntas01')
---
+++ pages/preguntas02.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+from deta import Deta
+import random
+import time
+
+import requests
+from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie_spinner
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+deta = Deta(st.secrets["deta_key"])
+
+datper = st.session_state['datper']
+datrpre = st.session_state['datrpre']
+precon = st.session_state['precon']
+datperXdon = st.session_state['datperXdon']
+dones = st.session_state['dones']
+
+lottie_url_processing = 'https://assets8.lottiefiles.com/packages/lf20_tmnc73b6.json'
+lottie_processing = load_lottieurl(lottie_url_processing)
+
+
+#st.write(datper)
+# st.write(len(datrpre))
+#st.write(precon)
+#st.write(datperXdon)
+#st.latex('Evaluado: '+datper[1]+'   ---   Evaluador: '+datper[0])
+#st.latex('Evaluado: '+datper[1])
+#st.latex('Relación: '+datper[2])
+#st.latex('Parentesco: '+datper[3])
+#st.info(datper[1]+ '  	🔀 	🔀 	🔀  '+datper[3])
+col1, col2,col3 = st.columns(3)
+col1.info(datper[1])
+col2.warning('🔀 	   🔀 	    🔀🔀 	   🔀 	    🔀')
+col3.info(datper[3])
+st_lottie(lottie_processing, key='processing')
+if (len(datrpre))>0:
+    with st.empty():
+        for seconds in range(1):
+            st.write(f"⏳")
+            time.sleep(1)
+        st.write("✔️")
+        
+        st.session_state['selpre'] = random.choice(datrpre)
+        switch_page('espera1')
+else:
+    regDones = {
+	"Aconsejar": [bus[1] for bus in datperXdon if bus[0]=='Aconsejar'][0],
+	"Administración": [bus[1] for bus in datperXdon if bus[0]=='Administración'][0],
+	"Apostolado": [bus[1] for bus in datperXdon if bus[0]=='Apostolado'][0],
+	"Ayudas": [bus[1] for bus in datperXdon if bus[0]=='Ayudas'][0],
+	"Comunicación Creativa": [bus[1] for bus in datperXdon if bus[0]=='Comunicación Creativa'][0],
+	"Conocimiento": [bus[1] for bus in datperXdon if bus[0]=='Conocimiento'][0],
+	"Dar": [bus[1] for bus in datperXdon if bus[0]=='Dar'][0],
+	"Discernimiento": [bus[1] for bus in datperXdon if bus[0]=='Discernimiento'][0],
+	"Enseñanza": [bus[1] for bus in datperXdon if bus[0]=='Enseñanza'][0],
+	"Evangelismo": [bus[1] for bus in datperXdon if bus[0]=='Evangelismo'][0],
+	"Exhortación": [bus[1] for bus in datperXdon if bus[0]=='Exhortación'][0],
+	"Fe": [bus[1] for bus in datperXdon if bus[0]=='Fe'][0],
+	"Habilidad en un oficio o arte": [bus[1] for bus in datperXdon if bus[0]=='Habilidad en un oficio o arte'][0],
+	"Hospitalidad": [bus[1] for bus in datperXdon if bus[0]=='Hospitalidad'][0],
+	"Interpretación de lenguas": [bus[1] for bus in datperXdon if bus[0]=='Interpretación de lenguas'][0],
+	"Lenguas": [bus[1] for bus in datperXdon if bus[0]=='Lenguas'][0],
+	"Liderazgo": [bus[1] for bus in datperXdon if bus[0]=='Liderazgo'][0],
+	"Milagros": [bus[1] for bus in datperXdon if bus[0]=='Milagros'][0],
+	"Misericordia": [bus[1] for bus in datperXdon if bus[0]=='Misericordia'][0],
+	"Pastor-maestro": [bus[1] for bus in datperXdon if bus[0]=='Pastor-maestro'][0],
+	"Profecía": [bus[1] for bus in datperXdon if bus[0]=='Profecía'][0],
+	"Sabiduría": [bus[1] for bus in datperXdon if bus[0]=='Sabiduría'][0],
+	"Sanidades": [bus[1] for bus in datperXdon if bus[0]=='Sanidades'][0],
+	"evaluado": datper[0],
+	"evaluador": datper[1],
+	"fechayhora": "-",
+	"parentesco": datper[3],
+	"relacion": datper[2],
+    }
+    donesDB = deta.Base('dones')
+    donesDB.put(regDones)
+    st.write('----')
+    #st.stop()
+	#st.stop()
+    #switch_page('salidapre')
+    switch_page('preguntasFin')
+
