5aed6d6db513336bef7d0885c12805da39393987
pjluispb <124328519+pjluispb@users.noreply.github.com>
2023-06-21 15:06:25 -0400

    Update takepho03.py

--- pages/takepho03.py
+++ pages/takepho03.py
@@ -9,4 +9,4 @@ if img_file_buffer is not None:
     bytes_data = img_file_buffer.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
     # Save the image to disk
-    cv2.imwrite('c:/users/user/Desktop/python/prof/images/ima05.jpg', cv2_img)
+    cv2.imwrite('ima05.jpg', cv2_img)
