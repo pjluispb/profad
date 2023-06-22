7dd8e87a3cd27aab1bbfc056b623a300296d297e
pjluispb <124328519+pjluispb@users.noreply.github.com>
2023-06-21 21:23:25 -0400

    Update takepho03.py

--- pages/takepho03.py
+++ pages/takepho03.py
@@ -1,12 +1,12 @@
 import streamlit as st
-import cv2
-import numpy as np
+from deta import Deta
+import io
 
-img_file_buffer = st.camera_input("Take a picture")
+deta = Deta(st.secrets.deta_key)
+
+img_file_buffer = st.camera_input("Toma una foto")
 
 if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
     bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-    # Save the image to disk
-    cv2.imwrite('ima05.jpg', cv2_img)
+    photos = deta.Drive(name='phot01')
+    photos.put('ima12.jpg', io.BytesIO(bytes_data))
