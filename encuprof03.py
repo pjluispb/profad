144e9c0b0c29e0e2cb786436608db644d68cecfc
pjluispb <124328519+pjluispb@users.noreply.github.com>
2023-07-11 15:00:23 -0400

    Update encuprof03.py

--- encuprof03.py
+++ encuprof03.py
@@ -99,7 +99,9 @@ with st.form(key='miform'):
         fec_bautismo_agua = st.date_input('Fecha de Bautismo en agua', min_value=datetime.date(1940,1,1))
         fec_bautismo_Espiritu = st.date_input('Fecha de Bautismo en el Espiritu Santo', min_value=datetime.date(1940,1,1))
         testimonio = st.text_area('compartenos tu breve testimonio', '''  ''')
-        # padeInfancia = st_toggleswitch("Padecimientos de Infancia")
+        padeInfancia = st_toggleswitch("Padecimientos de Infancia")
+        if padeInfancia:
+            st.write("padeInfancia")
         # enfermedadVenerea = st_toggleswitch("Enfermedades venéreas")
 
 
