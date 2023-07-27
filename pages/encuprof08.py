98992200760d7531859ebd936897e23f7b9ef068
pjluispb <124328519+pjluispb@users.noreply.github.com>
2023-07-27 11:54:24 -0400

    Update encuprof08.py

--- pages/encuprof08.py
+++ pages/encuprof08.py
@@ -250,7 +250,7 @@ with st.expander('Datos acerca del trabajo'):
 
 with st.expander('Datos acerca de su testimonio'):
     with st.form(key='dattesti'):
-        fec_conversion = st.date_input('Fecha de Conversión', min_value=datetime.date(1940,1,1), value=datetime.datetime.strptime(valores['fec_conversion'], "%d/%m/%Y"), max_value=datetime.datetime.now())
+        fec_conversion = st.date_input('Fecha de Conversión', min_value=datetime.date(1940,1,1), value=datetime.datetime.strptime(valores['fec_conversion'], "%D/%mM/%Y"), max_value=datetime.datetime.now(), format="DD/MM/YYYY",)
         fec_bautismo_agua = st.date_input('Fecha de Bautismo en agua', min_value=datetime.date(1940,1,1), value=datetime.datetime.strptime(valores['fec_bautismo_agua'], "%d/%m/%Y"), max_value=datetime.datetime.now())
         fec_bautismo_Espiritu = st.date_input('Fecha de Bautismo en el Espiritu Santo', min_value=datetime.date(1940,1,1), value=datetime.datetime.strptime(valores['fec_bautismo_Espiritu'], "%d/%m/%Y"), max_value=datetime.datetime.now())
         testimonio = st.text_area('Compártenos tu testimonio de salvación', value=valores['testimonio'])
